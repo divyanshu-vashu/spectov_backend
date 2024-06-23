a5efb9211b3cada505ca67f6a1c032e873e2af90
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2024-06-23 10:38:29 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,33 @@
+/*const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://codesharecode:p0tq33ps5cbWKZga@cluster0.dtvlco8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useCreateIndex: true,
+     // useFindAndModify: false
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.log('Error connecting to MongoDB:', error);
+  }
+};
+
+module.exports = connectDB;*/
+
+const mongoose = require("mongoose");
+
+module.exports = () => {
+	const connectionParams = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
+	try {
+		mongoose.connect('mongodb+srv://codesharecode:p0tq33ps5cbWKZga@cluster0.dtvlco8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', connectionParams);
+		console.log("Connected to database successfully");
+	} catch (error) {
+		console.log(error);
+		console.log("Could not connect database!");
+	}
+};
