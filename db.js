2669b7b85150217661d6e5a7d379fe949d085be2
Divyanshu Singh <89933176+divyanshu-vashu@users.noreply.github.com>
2024-07-08 16:13:46 +0530

    Update db.js

--- db.js
+++ db.js
@@ -16,18 +16,36 @@ const connectDB = async () => {
 
 module.exports = connectDB;*/
 
+// const mongoose = require("mongoose");
+
+// module.exports = () => {
+// 	const connectionParams = {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	};
+// 	try {
+// 		mongoose.connect('mongodb+srv://codesharecode:p0tq33ps5cbWKZga@cluster0.dtvlco8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', connectionParams);
+// 		console.log("Connected to database successfully");
+// 	} catch (error) {
+// 		console.log(error);
+// 		console.log("Could not connect database!");
+// 	}
+// };
+
+
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 module.exports = () => {
-	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	};
-	try {
-		mongoose.connect('mongodb+srv://codesharecode:p0tq33ps5cbWKZga@cluster0.dtvlco8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', connectionParams);
-		console.log("Connected to database successfully");
-	} catch (error) {
-		console.log(error);
-		console.log("Could not connect database!");
-	}
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+    try {
+        mongoose.connect(process.env.MONGODB_URI, connectionParams);
+        console.log("Connected to database successfully");
+    } catch (error) {
+        console.log(error);
+        console.log("Could not connect database!");
+    }
 };
